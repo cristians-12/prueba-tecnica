fc727aea66afb7ad7496b0fc1973f3153b23c54d
Eggward12 <cristian.sanchez.casd@gmail.com>
2023-09-16 02:12:53 -0500

    Funciona

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,42 @@ import { useEffect, useState } from 'react'
 
 export function App () {
   const [fact, setFact] = useState(null)
-  const ENDPOINT_IMG_GATO = 'https://cataas.com/cat/says/{$palabra}?size=50&color={$color}&json=true'
+  const [valor, setValor] = useState('')
+  const [imageUrl, setimageUrl] = useState('')
+  //   const ENDPOINT_IMG_GATO =
+  // 'https://cataas.com/cat/says/{$palabra}?size=50&color={$color}&json=true'
   const ENDPOINT_FACT = 'https://catfact.ninja/fact'
 
   useEffect(() => {
     fetch(ENDPOINT_FACT)
-      .then(res => res.json())
-      .then(data => setFact(data.fact))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+
+        // const palabra = fact.split(' ')[0]
+        fetch(`https://cataas.com/cat/says/${valor}?size=50&color={$color}&json=true`)
+          .then(res => res.json())
+          .then(respuesta => {
+            const { url } = respuesta
+            console.log(url)
+            setimageUrl(`https://cataas.com${url}`)
+          })
+      })
+  }, [valor])
+
+  const nuevoFact = (e) => {
+    setValor(e.target.value)
+    // console.log(valor)
+  }
 
   return (
     <>
       <h1>App de gatos con API</h1>
       {fact && <p>{fact}</p>}
-
+      {imageUrl && <img src={imageUrl} alt='cat' />}
+      <br />
+      <input type='text' name='palabra' value={valor} onChange={nuevoFact} />
     </>
   )
 }
